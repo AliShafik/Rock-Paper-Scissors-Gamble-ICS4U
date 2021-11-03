ffe4c711fc2dec3ba6352c702435fbce33ac0e94
AliShafik <92734614+AliShafik@users.noreply.github.com>
2021-11-03 00:30:50 -0400

    Version 1 of Game

---
+++ Main.java
@@ -0,0 +1,30 @@
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import javax.swing.*;
+
+class Main {
+
+  public static boolean playAgain;
+
+  public static void main(String[] args) {
+    
+    new MainMenu();
+
+    /*do{
+      new MainMenu();
+      put other classes here
+      user clicks play playAgain
+      sets playagain to true
+      user clicks quit game
+      sets playagain false
+
+    }while(playAgain==true);*/
+    
+
+
+  }
+}
